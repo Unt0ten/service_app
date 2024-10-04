3a6805b4d4a6b5b65afcf88d7a2347512c19b459
Alexandr Sakhatov <sakhatoff@gmail.com>
2024-10-04 13:29:33 +0700

    optimiz celery tasks

--- service/services/models.py
+++ service/services/models.py
@@ -2,11 +2,23 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
+from services.tasks import set_price
 
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Name: {self.name}'
@@ -24,6 +36,17 @@ class Plan(models.Model):
         default=0,
         validators=[MaxValueValidator(100)],
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Plan: {self.plan_type}'
@@ -45,6 +68,7 @@ class Subscription(models.Model):
         related_name='subscriptions',
         on_delete=models.PROTECT,
     )
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f'Client: {self.client.company_name}; ' \
